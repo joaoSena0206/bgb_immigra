485bf33260132094ccf50a823dd39b03ed1ef528
João Sena <joaosena0206@gmail.com>
2024-09-23 18:57:23 -0300

    Adiciona select customizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+function trocarTexto(indice, novoIndice, div, direcao) {
+    const slideOutClass = direcao === 'direita' ? 'slide-out-right' : 'slide-out-left';
+    const slideInClass = direcao === 'direita' ? 'slide-in-right' : 'slide-in-left';
+
+    titulo_servico.classList.add(slideOutClass);
+
+    const servicoAtual = document.querySelector(".div_servicos2#servico_" + indice);
+    servicoAtual.classList.add(slideOutClass);
+
+    setTimeout(() => {
+        titulo_servico.textContent = servicos[novoIndice];
+
+        titulo_servico.classList.remove(slideOutClass);
+        titulo_servico.classList.add(slideInClass);
+
+        setTimeout(() => {
+            titulo_servico.classList.remove(slideInClass);
+        }, 300);
+
+        servicoAtual.style.display = "none";
+        div.style.display = "flex";
+
+        servicoAtual.classList.remove(slideOutClass);
+        div.classList.add(slideInClass);
+
+        setTimeout(() => {
+            div.classList.remove(slideInClass);
+        }, 300);
+
+    }, 300);
+}
+
+
 let servicos = ["Imigração", "Vistos", "Documentos", "Jurídicos", "Suporte e Consultoria"];
 
 const setaDireita = document.querySelector(".div_setas__img--seta_direita");
@@ -9,18 +42,12 @@ setaDireita.addEventListener("click", function () {
     for (let i = 0; i < servicos.length; i++) {
         if (titulo_servico.textContent == servicos[i]) {
             if (i != servicos.length - 1) {
-                titulo_servico.textContent = servicos[i + 1];
-
-                divServico[i].style.display = "none";
-                divServico[i + 1].style.display = "flex";
+                trocarTexto(i, i + 1, divServico[i + 1], 'direita');
 
                 break;
             }
             else if (i == servicos.length - 1) {
-                titulo_servico.textContent = servicos[0];
-
-                divServico[i].style.display = "none";
-                divServico[0].style.display = "flex";
+                trocarTexto(i, 0, divServico[0], 'direita');
 
                 break;
             }
@@ -32,21 +59,22 @@ setaEsquerda.addEventListener("click", function () {
     for (let i = 0; i < servicos.length; i++) {
         if (titulo_servico.textContent == servicos[i]) {
             if (i != 0) {
-                titulo_servico.textContent = servicos[i - 1];
-
-                divServico[i].style.display = "none";
-                divServico[i - 1].style.display = "flex";
+                trocarTexto(i, i - 1, divServico[i - 1], 'esquerda');
 
                 break;
             }
             else if (i == 0) {
-                titulo_servico.textContent = servicos[servicos.length - 1];
-
-                divServico[i].style.display = "none";
-                divServico[servicos.length - 1].style.display = "flex";
+                trocarTexto(i, servicos.length - 1, divServico[servicos.length - 1], 'esquerda');
 
                 break;
             }
         }
     }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const idiomaSelect = new Choices('#idioma-select', {
+        searchEnabled: false,  
+        itemSelectText: '',    
+    });
 });
